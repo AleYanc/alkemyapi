0982df6d6e16ac1f1d0dac18b48f8c0663c0b2ab
AleYanc <aleyanc2000@gmail.com>
2021-10-29 14:52:58 -0300

    Fixed authenticity token error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
   include ActionController::HttpAuthentication::Token
   def authenticate_user
     token, _options = token_and_options(request)
